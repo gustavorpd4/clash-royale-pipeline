932a9d2f58919f6244ef63c8b122c671063c6bd3
Gustavo <gustavorpd4@gmail.com>
2026-06-30 22:54:18 -0500

    test api

---
+++ test_api.py
@@ -0,0 +1,14 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("CLASH_API_TOKEN")
+headers = {"Authorization": f"Bearer {TOKEN}"}
+
+url = "https://api.clashroyale.com/v1/cards"
+response = requests.get(url, headers=headers)
+
+print("Status:", response.status_code)
+print("Respuesta completa:", response.json())
